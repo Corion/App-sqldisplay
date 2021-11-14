9a02858682287c3f237ce815dce150e52ceeeda5
Max Maischein <corion@corion.net>
2021-11-14 22:11:20 +0100

    Add build infrastructure

---
+++ lib/App/sqldisplay_http.pm
@@ -0,0 +1,11 @@
+package App::sqldisplay_http;
+use strict;
+our $VERSION = '0.01';
+
+=head1 NAME
+
+App::sqldisplay_http - Display results of SQL queries against a spreadsheet in HTML
+
+=cut
+
+1;
