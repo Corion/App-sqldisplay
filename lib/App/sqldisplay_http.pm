e0a3511acfc37e0790a95623abe537fdc7888cb3
Max Maischein <corion@corion.net>
2021-11-15 16:59:46 +0100

    Update description

--- lib/App/sqldisplay_http.pm
+++ lib/App/sqldisplay_http.pm
@@ -4,7 +4,7 @@ our $VERSION = '0.01';
 
 =head1 NAME
 
-App::sqldisplay_http - Display results of SQL queries against a spreadsheet in HTML
+App::sqldisplay_http - Display live results of SQL queries against a spreadsheet in HTML
 
 =cut
 
